d35a47e5b65d7d1d1f6c8c72f7d2a3601454a3a4
SamiJelassi <samijlassi2909@gmail.com>
2026-05-16 20:23:37 +0200

    wiqar

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false
+  },
+  server: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://54.37.159.225:6000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
